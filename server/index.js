60fc2639f11e78eb9811897b074333cbf3f28e86
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2023-09-07 23:55:00 +0530

    fixed password condition in signin route

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 app.use(express.json());
 let user_id_c=0;
-
+var jwt = require("jsonwebtoken");
 const JWT_SECRET = "secret";
 const USERS=[]
 const PROBLEMS =[
@@ -117,17 +117,18 @@ app.post('/signup',(req, res)=> {
     });
 });
 
-app.post('/login', (req, res)=> {
+app.post('/signin', (req, res)=> {
 
 
     const email=req.body.email;
     const password=req.body.password;
     const user=USERS.find((x)=>x.email===email);
 
+
     if(!user){
         return res.status(403).json({msg:"User not found."});
     }
-    if(!password){
+    if(user.password!==password){
         return res.status(403).json({msg:"Incorrect password."})
     }
     const token=jwt.sign(
